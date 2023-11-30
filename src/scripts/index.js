34f657c49da8045bb6b76e77213694bc4c3ef026
Achmad Ata <132084409+Adlrsyadu@users.noreply.github.com>
2023-11-30 17:40:31 +0700

    Export File Form Dekstop

---
+++ src/scripts/index.js
@@ -0,0 +1,24 @@
+import 'regenerator-runtime';
+import '../styles/main.css';
+import '../styles/detail.css';
+import '../styles/comments.css';
+import App from './views/app';
+import swRegister from './utils/sw-register';
+import WebSocketInitiator from './utils/websocket-initiator';
+import CONFIG from './globals/config';
+
+const app = new App({
+  button: document.querySelector('#hamburger'),
+  drawer: document.querySelector('#drawer'),
+  content: document.querySelector('#restaurant-list'),
+});
+
+window.addEventListener('hashchange', () => {
+  app.renderPage();
+});
+
+window.addEventListener('load', () => {
+  app.renderPage();
+  swRegister();
+  WebSocketInitiator.init(CONFIG.WEB_SOCKET_SERVER);
+});
